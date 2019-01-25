cb6c9a74666d0015ea1ba7385271d952989abe43
Jin Pae <jin@jinpae.com>
2019-01-24 23:01:42 -0800

    Attempt to remove flickring when loading the devotional page

--- src/templates/devotional.js
+++ src/templates/devotional.js
@@ -5,40 +5,14 @@ import Page from '../components/Page/Page';
 
 import styles from './devotional.module.scss';
 
-export const DevotionalPageTemplate = ({ title, passage, content }) => {
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long'
-  };
-  const date = new Date().toLocaleDateString('en-US', options);
-
-  return (
-    <Page title="Daily Devotion" className={styles.component}>
-      <Helmet title={`${title} | Daily Devotion`} />
-      <article className={styles.article}>
-        <p className={styles.date}>{date}</p>
-        <h2 className={styles.articleTitle}>{passage}</h2>
-        {/* <div
-          className={styles.articleContent}
-          dangerouslySetInnerHTML={{ __html: content }}
-        /> */}
-        <div className={styles.articleContent}>Passage placeholder</div>
-      </article>
-    </Page>
-  );
-};
-
-class DevotionalPage extends React.Component {
+export class DevotionalPageTemplate extends React.Component {
   state = {
-    passages: undefined
+    passage: '',
+    text: 'Placeholder text...'
   };
 
   componentDidMount() {
-    const { markdownRemark: page } = this.props.data;
-
-    fetch(page.frontmatter.passagesUrl)
+    fetch(this.props.passagesUrl)
       .then(response => response.json())
       .then(passages => {
         const today = new Date();
@@ -50,22 +24,39 @@ class DevotionalPage extends React.Component {
   }
 
   render() {
-    const { markdownRemark: page, html } = this.props.data;
-    const { passage } = this.state;
-
-    if (!passage) {
-      return null;
-    }
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long'
+    };
+    const date = new Date().toLocaleDateString('en-US', options);
 
     return (
-      <DevotionalPageTemplate
-        title={page.frontmatter.title}
-        content={html}
-        passage={this.state.passage}
-      />
+      <Page title="Daily Devotion" className={styles.component}>
+        <Helmet title={`${this.props.title} | Daily Devotion`} />
+        <article className={styles.article}>
+          <p className={styles.date}>{date}</p>
+          <h2 className={styles.articleTitle}>{this.state.passage}</h2>
+          {/* <div
+            className={styles.articleContent}
+            dangerouslySetInnerHTML={{ __html: content }}
+          /> */}
+          <div className={styles.articleContent}>{this.state.text}</div>
+        </article>
+      </Page>
     );
   }
 }
+
+const DevotionalPage = ({ data }) => {
+  const { markdownRemark: page } = data;
+  const {
+    frontmatter: { title, passagesUrl }
+  } = page;
+
+  return <DevotionalPageTemplate title={title} passagesUrl={passagesUrl} />;
+};
 
 DevotionalPage.propTypes = {
   data: PropTypes.object.isRequired
