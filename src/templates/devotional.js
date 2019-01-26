f68e2f1b408c423068b1bc4c5b9b387139d85e8e
Jin Pae <jin@jinpae.com>
2019-01-26 15:46:57 -0800

    Add previous and next nav for devotional page

--- src/templates/devotional.js
+++ src/templates/devotional.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 import Page from '../components/Page/Page';
 
 import styles from './devotional.module.scss';
 
 const ESV_API_ENDPOINT = `https://api.esv.org/v3/passage/html/`;
 const API_KEY = `e774b4f993c184ef6c9a3165f672089e558a6add`;
+const DAY = 60 * 60 * 24 * 1000;
 
 export class DevotionalPageTemplate extends React.Component {
   state = {
@@ -14,13 +16,27 @@ export class DevotionalPageTemplate extends React.Component {
     text: ''
   };
 
+  static defaultProps = {
+    date: new Date()
+  };
+
   componentDidMount() {
+    this.fetchPassageText();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.date !== prevProps.date) {
+      this.fetchPassageText();
+    }
+  }
+
+  fetchPassageText() {
     fetch(this.props.passagesUrl)
       .then(response => response.json())
       .then(passages => {
-        const today = new Date();
-        const passagesForCurrentMonth = passages[today.getMonth()];
-        const passage = passagesForCurrentMonth[today.getDate() - 1];
+        const { date } = this.props;
+        const passagesForCurrentMonth = passages[date.getMonth()];
+        const passage = passagesForCurrentMonth[date.getDate() - 1];
         this.setState({ passage }, () => this.getPassageText(passage));
       })
       .catch(error => console.warn('Error', error.message));
@@ -49,32 +65,90 @@ export class DevotionalPageTemplate extends React.Component {
       weekday: 'long'
     };
 
-    const date = new Date().toLocaleDateString('en-US', options);
+    const { passage, text } = this.state;
+    const { date } = this.props;
 
     return (
       <Page title="Daily Devotion" className={styles.component}>
         <Helmet title={`${this.props.title} | Daily Devotion`} />
         <article className={styles.article}>
-          <h2 className={styles.articleTitle}>{this.state.passage}</h2>
-          <p className={styles.date}>{date}</p>
+          <h2 className={styles.articleTitle}>{passage}</h2>
+          <p className={styles.date}>
+            {date.toLocaleDateString('en-US', options)}
+          </p>
           <div
             className={styles.articleContent}
-            dangerouslySetInnerHTML={{ __html: this.state.text }}
+            dangerouslySetInnerHTML={{ __html: text }}
           />
+          <div className={styles.passageNav}>
+            <Link
+              rel="prev"
+              to={{
+                pathname: '/devotional/',
+                state: {
+                  date: new Date(date.getTime() - DAY)
+                }
+              }}
+            >
+              ← Previous
+            </Link>
+            <Link
+              rel="next"
+              to={{
+                pathname: '/devotional/',
+                state: {
+                  date: new Date(date.getTime() + DAY)
+                }
+              }}
+            >
+              Next →
+            </Link>
+          </div>
         </article>
       </Page>
     );
   }
 }
 
-const DevotionalPage = ({ data }) => {
-  const { markdownRemark: page } = data;
-  const {
-    frontmatter: { title, passagesUrl }
-  } = page;
+class DevotionalPage extends React.Component {
+  state = {
+    date: new Date()
+  };
 
-  return <DevotionalPageTemplate title={title} passagesUrl={passagesUrl} />;
-};
+  componentDidMount() {
+    const { state } = this.props.location;
+
+    if (state && state.date) {
+      this.setState({ date: new Date() });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { state: currentState } = this.props.location;
+    const { state: prevState } = prevProps.location;
+
+    if (currentState !== prevState) {
+      const date = (currentState && currentState.date) || new Date();
+      this.setState({ date: date });
+    }
+  }
+
+  render() {
+    const { markdownRemark: page } = this.props.data;
+    const {
+      frontmatter: { title, passagesUrl }
+    } = page;
+    const { date } = this.state;
+
+    return (
+      <DevotionalPageTemplate
+        title={title}
+        passagesUrl={passagesUrl}
+        date={date}
+      />
+    );
+  }
+}
 
 DevotionalPage.propTypes = {
   data: PropTypes.object.isRequired
