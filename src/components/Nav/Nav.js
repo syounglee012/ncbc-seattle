78f45c67f2690ec348dbd756d1fe5e8943122281
Jin Pae <jin@jinpae.com>
2019-02-15 23:44:38 -0800

    Add media queries for nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,22 +4,29 @@ import Link from 'gatsby-link';
 import styles from './Nav.module.scss';
 
 const Nav = () => (
-  <nav className={styles.component}>
-    <ul>
-      <li>
-        <Link to="/about/">About</Link>
-      </li>
-      <li>
-        <Link to="/ministries/">Ministries</Link>
-      </li>
-      <li>
-        <Link to="/devotional/">Devotional</Link>
-      </li>
-      <li>
-        <Link to="/location/">Location</Link>
-      </li>
-    </ul>
-  </nav>
+  <div className={styles.component}>
+    <button className={styles.linkToggle} aria-label="Nav">
+      <span className={styles.hamburgerLine} />
+      <span className={styles.hamburgerLine} />
+      <span className={styles.hamburgerLine} />
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/about/">About</Link>
+        </li>
+        <li>
+          <Link to="/ministries/">Ministries</Link>
+        </li>
+        <li>
+          <Link to="/devotional/">Devotional</Link>
+        </li>
+        <li>
+          <Link to="/location/">Location</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Nav;
